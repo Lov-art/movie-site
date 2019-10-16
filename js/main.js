6be08282df639e5cdd8c680427607300b7d923b4
Artem <lyovinn.artem@gmail.com>
2019-10-16 12:35:44 +0300

    4th homework

--- js/main.js
+++ js/main.js
@@ -1,97 +1,512 @@
-function Movie (title, director, actors, synopsis ) {
-	this.title = title;
-	this.director = director;
-	this.actors = actors;
-	this.synopsis = synopsis;
-
-	this.getTitle = function (){
-		var str=" ";
-
-		for (var i = 0; i < this.title.length ; i++) {
-				str += this.title[i].getName()
-		}
-		return str;
-	}
-
-	this.getDirector = function (){
-		var str=" ";
-
-		for (var i = 0; i < this.director.length ; i++) {
-				str += this.director[i].getName()
-		}
-		return str;	
-	}
-
-	this.getActors = function (){
-		var str=" ";
-
-		for (var i = 0; i < this.actors.length ; i++) {
-				str += this.actors[i].getName()
-		}
-		return str;
+//  function Movie (data ) {
+// 	this.title = data.Title;
 	
-	}
+// 	this.synopsis = data.Synopsis;
+
+// }
+// Movie.prototype = {
+// 		getTitle: function (){
+// 		var str=" ";
+
+// 		for (var i = 0; i < this.title.length ; i++) {
+// 				str += this.title[i].getName()
+// 		}
+// 		return str;
+// 		} ,
+
+// 		getDirector: function (){
+// 		var str=" ";
+
+// 		for (var i = 0; i < this.director.length ; i++) {
+// 				str += this.director[i].getName()
+// 		}
+// 		return str;	
+// 		} ,
+
+// 		getActors: function (){
+// 		var str=" ";
+
+// 		for (var i = 0; i < this.actors.length ; i++) {
+// 				str += this.actors[i].getName()
+// 		}
+// 		return str;
+	
+// 		} ,
+
+// 		getSynopsis: function(){
+// 		var str=" ";
+
+// 		for (var i = 0; i < this.synopsis.length ; i++) {
+// 				str += this.synopsis[i].getName()
+// 		}
+// 		return str;
+// 		}
+// 	}
 
 
-	this.getSynopsis = function(){
-		var str=" ";
+// function actors ( name ){
 
-		for (var i = 0; i < this.synopsis.length ; i++) {
-				str += this.synopsis[i].getName()
-		}
-		return str;
-	}
+// 	this.name = name ;
+// 	this.getName = function(){
+// 		return this.name;
+// 	}
+// }
 
+// function director ( name ){
 
+// 	this.name = name ;
+// 	this.getName = function(){
+// 		return this.name;
+// 	}
 
 	
+// }		
+
+
+// function title ( name ){
+
+// 	this.name = name ;
+// 	this.getName = function(){
+// 		return this.name;
+// 	}
+
+// }	
+
+// function synopsis (name){
+// 	this.name = name ;
+// 	this.getName = function(){
+// 		return this.name;
+// 	}
+// }
+var Movies = [
+  {
+    "ID": "HO00000199",
+    "ShortCode": "",
+    "Title": "ІЗОЛЯЦІЯ ",
+    "Rating": "16+",
+    "RatingDescription": "",
+    "Synopsis": "Лінда опиняється на відлюдному острові разом з убивцею. Вона змушена зробити розтин тіла, щоб знайти підказку для порятунку доньки коронера Герцфілда.",
+    "ShortSynopsis": "",
+    "HOFilmCode": "F000000244",
+    "CorporateFilmId": "",
+    "RunTime": 90,
+    "OpeningDate": "2019-08-22T00:00:00",
+    "GraphicUrl": "",
+    "FilmNameUrl": "2018",
+    "TrailerUrl": "",
+    "AdditionalUrls": [],
+    "IsComingSoon": true,
+    "IsScheduledAtCinema": false,
+    "TitleAlt": "Abgeschnitten ",
+    "RatingAlt": "",
+    "RatingDescriptionAlt": "",
+    "SynopsisAlt": "Німеччина",
+    "ShortSynopsisAlt": "Ziegler Film GmbH & Co. KG; Syrreal Entertainment GmbH",
+    "WebsiteUrl": "",
+    "GenreId": "0000000008",
+    "EDICode": null,
+    "TwitterTag": "українська",
+    "TitleTranslations": [],
+    "SynopsisTranslations": [],
+    "ShortSynopsisTranslations": [],
+    "RatingDescriptionTranslations": [],
+    "CustomerRatingStatistics": {
+      "RatingCount": 0,
+      "AverageScore": null
+    },
+    "CustomerRatingTrailerStatistics": {
+      "RatingCount": 0,
+      "RatingCountLiked": 0
+    },
+    "FilmWebId": ""
+  },
+  {
+    "ID": "HO00000205",
+    "ShortCode": "",
+    "Title": "ФОРСАЖ: ГОББС ТА ШОУ",
+    "Rating": "12+",
+    "RatingDescription": "",
+    "Synopsis": "Закляті супротивники спецагент Гоббс і злочинець Шоу живуть за полярними принципами і ніколи не будуть довіряти один одному. Та у спін-офф культової франшизи «Форсаж» вони змушені об’єднатися, щоб захистити світ від нищівної біологічної атаки.",
+    "ShortSynopsis": "http://bhfilms.com.ua/#tabr1nrS8T",
+    "HOFilmCode": "F000000250",
+    "CorporateFilmId": "",
+    "RunTime": 140,
+    "OpeningDate": "2019-08-01T00:00:00",
+    "GraphicUrl": "",
+    "FilmNameUrl": "2019",
+    "TrailerUrl": "https://youtu.be/-qLiSKzkzcc",
+    "AdditionalUrls": [],
+    "IsComingSoon": true,
+    "IsScheduledAtCinema": false,
+    "TitleAlt": "Fast & Furious presents: Hobbs & Shaw",
+    "RatingAlt": "",
+    "RatingDescriptionAlt": "",
+    "SynopsisAlt": "США",
+    "ShortSynopsisAlt": "Universal Pictures",
+    "WebsiteUrl": "",
+    "GenreId": "0000000019",
+    "EDICode": null,
+    "TwitterTag": "українська",
+    "TitleTranslations": [],
+    "SynopsisTranslations": [],
+    "ShortSynopsisTranslations": [],
+    "RatingDescriptionTranslations": [],
+    "CustomerRatingStatistics": {
+      "RatingCount": 0,
+      "AverageScore": null
+    },
+    "CustomerRatingTrailerStatistics": {
+      "RatingCount": 0,
+      "RatingCountLiked": 0
+    },
+    "FilmWebId": ""
+  },
+  {
+    "ID": "HO00000220",
+    "ShortCode": "",
+    "Title": "КОРОЛЕВА СЕРДЕЦЬ",
+    "Rating": "18+",
+    "RatingDescription": "",
+    "Synopsis": "В Анни вельми спокійне, розмірене життя. Вона щасливо перебуває у шлюбі зі своїм чоловіком Пітером, у них є дві прекрасні доньки. Та й на роботі в Анни все відмінно – вона успішний затребуваний юрист. Усе в її житті йде саме так, як потрібно, усе ідеально. У цей бездоганний механізм доля вирішила підкинути один гвинтик – Густава, сина Пітера від попереднього шлюбу. Він не дуже любить батька, але сім'я повинна його прийняти на деякий час. Хлопець зміг досить швидко адаптуватися до нового середовища, почав шукати друзів. Й Анна змогла полюбити нового сина. Тільки от не материнською любов'ю, а чимось більш інтимним. Між ними почалася пристрасть, яка поки прихована від очей Пітера. Цей порив може зруйнувати до того ідеальне життя.",
+    "ShortSynopsis": "",
+    "HOFilmCode": "F000000265",
+    "CorporateFilmId": "",
+    "RunTime": 130,
+    "OpeningDate": "2019-08-01T00:00:00",
+    "GraphicUrl": "",
+    "FilmNameUrl": "2019",
+    "TrailerUrl": "",
+    "AdditionalUrls": [],
+    "IsComingSoon": true,
+    "IsScheduledAtCinema": false,
+    "TitleAlt": "Dronningen ",
+    "RatingAlt": "",
+    "RatingDescriptionAlt": "",
+    "SynopsisAlt": "Швеція, Данія",
+    "ShortSynopsisAlt": "Danmarks Radio, Det Danske Filminstitut",
+    "WebsiteUrl": "",
+    "GenreId": "0000000004",
+    "EDICode": null,
+    "TwitterTag": "українська",
+    "TitleTranslations": [],
+    "SynopsisTranslations": [],
+    "ShortSynopsisTranslations": [],
+    "RatingDescriptionTranslations": [],
+    "CustomerRatingStatistics": {
+      "RatingCount": 0,
+      "AverageScore": null
+    },
+    "CustomerRatingTrailerStatistics": {
+      "RatingCount": 0,
+      "RatingCountLiked": 0
+    },
+    "FilmWebId": ""
+  },
+  {
+    "ID": "HO00000222",
+    "ShortCode": "",
+    "Title": "ПАРАЗИТИ",
+    "Rating": "16+",
+    "RatingDescription": "",
+    "Synopsis": "У сім'ї чотирьох безробітних похмурі життєві перспективи. Ситуація змінюється, коли старшому синові пропонують високооплачуваний підробіток репетитором. З допомогою сестри він робить фальшиві документи про освіту та приходить до розкішного маєтку заможного бізнесмена. Справивши гарне враження, старший син дізнається про ще одну відкриту вакансію, і у нього з'являється дуже незвичайний план з працевлаштування сестри.\r\n",
+    "ShortSynopsis": "",
+    "HOFilmCode": "F000000267",
+    "CorporateFilmId": "",
+    "RunTime": 135,
+    "OpeningDate": "2019-08-01T00:00:00",
+    "GraphicUrl": "",
+    "FilmNameUrl": "2019",
+    "TrailerUrl": "https://youtu.be/KKNXGJIpnKY",
+    "AdditionalUrls": [],
+    "IsComingSoon": true,
+    "IsScheduledAtCinema": false,
+    "TitleAlt": "Gisaengchung ",
+    "RatingAlt": "",
+    "RatingDescriptionAlt": "",
+    "SynopsisAlt": "Південна корея",
+    "ShortSynopsisAlt": "CJ Entertainment",
+    "WebsiteUrl": "",
+    "GenreId": "0000000008",
+    "EDICode": null,
+    "TwitterTag": "українська",
+    "TitleTranslations": [],
+    "SynopsisTranslations": [],
+    "ShortSynopsisTranslations": [],
+    "RatingDescriptionTranslations": [],
+    "CustomerRatingStatistics": {
+      "RatingCount": 0,
+      "AverageScore": null
+    },
+    "CustomerRatingTrailerStatistics": {
+      "RatingCount": 0,
+      "RatingCountLiked": 0
+    },
+    "FilmWebId": ""
+  },
+  {
+    "ID": "HO00000223",
+    "ShortCode": "",
+    "Title": "ЗАМША",
+    "Rating": "16+",
+    "RatingDescription": "",
+    "Synopsis": "Сорокачотирьохрічний чоловік Жорж зовсім недавно придбав собі нову куртку, яка була зроблена з оленячої шкіри. Якщо сказати, що вона виглядає стильно, то це нічого не сказати. Із цього часу Жорж прямо-таки зрісся з цією курткою, вона стала для нього як рідна. А вона у свою чергу почала ним управляти, взяла під свій контроль. Жорж почав витрачати всі свої заощадження, поводиться абсолютно не так, як раніше. І все було би ще не так погано, якби чоловік робив шкоду тільки собі... Справи пішли куди далі – Жорж під впливом куртки почав убивати. Куди ще вона його заведе, абсолютно незрозуміло.",
+    "ShortSynopsis": "",
+    "HOFilmCode": "F000000268",
+    "CorporateFilmId": "",
+    "RunTime": 80,
+    "OpeningDate": "2019-08-01T00:00:00",
+    "GraphicUrl": "",
+    "FilmNameUrl": "2019",
+    "TrailerUrl": "",
+    "AdditionalUrls": [],
+    "IsComingSoon": true,
+    "IsScheduledAtCinema": false,
+    "TitleAlt": "Le daim",
+    "RatingAlt": "",
+    "RatingDescriptionAlt": "",
+    "SynopsisAlt": "Франція",
+    "ShortSynopsisAlt": "Arte France, Canal+, Nexus Factory",
+    "WebsiteUrl": "",
+    "GenreId": "0000000009",
+    "EDICode": null,
+    "TwitterTag": "ЗАМША",
+    "TitleTranslations": [],
+    "SynopsisTranslations": [],
+    "ShortSynopsisTranslations": [],
+    "RatingDescriptionTranslations": [],
+    "CustomerRatingStatistics": {
+      "RatingCount": 0,
+      "AverageScore": null
+    },
+    "CustomerRatingTrailerStatistics": {
+      "RatingCount": 0,
+      "RatingCountLiked": 0
+    },
+    "FilmWebId": ""
+  },
+  {
+    "ID": "HO00000224",
+    "ShortCode": "",
+    "Title": "КОРОЛЕВИ КРИМІНАЛУ",
+    "Rating": "16+",
+    "RatingDescription": "",
+    "Synopsis": "«Королеви криміналу» - це історія про трьох домогосподарок. Кеті (Мелісса МакКарті), Клер (Елізабет Мосс) і Рубі (Тіффані Хеддіш) живуть у нью-йоркському районі, який ще називають Пекельна кухня. Одного разу їхніх чоловіків-бандитів ФБР запроторює до в’язниці. Залишившись сам-на-сам зі своїми проблемами, жінки беруть керівництво ірландською мафією у власні руки і тепер завідують усім: від рекету до прибирання конкурентів...у прямому сенсі слова.",
+    "ShortSynopsis": "",
+    "HOFilmCode": "F000000269",
+    "CorporateFilmId": "",
+    "RunTime": 110,
+    "OpeningDate": "2019-08-08T00:00:00",
+    "GraphicUrl": "",
+    "FilmNameUrl": "2019",
+    "TrailerUrl": "https://youtu.be/q5i22NYXzqo",
+    "AdditionalUrls": [],
+    "IsComingSoon": true,
+    "IsScheduledAtCinema": false,
+    "TitleAlt": "The Kitchen",
+    "RatingAlt": "",
+    "RatingDescriptionAlt": "",
+    "SynopsisAlt": "США",
+    "ShortSynopsisAlt": "Warner Bros. Pictures, New Line Cinema",
+    "WebsiteUrl": "",
+    "GenreId": "0000000019",
+    "EDICode": null,
+    "TwitterTag": "КОРОЛЕВИ КРИМІНАЛУ",
+    "TitleTranslations": [],
+    "SynopsisTranslations": [],
+    "ShortSynopsisTranslations": [],
+    "RatingDescriptionTranslations": [],
+    "CustomerRatingStatistics": {
+      "RatingCount": 0,
+      "AverageScore": null
+    },
+    "CustomerRatingTrailerStatistics": {
+      "RatingCount": 0,
+      "RatingCountLiked": 0
+    },
+    "FilmWebId": ""
+  },
+  {
+    "ID": "HO00000227",
+    "ShortCode": "",
+    "Title": "БЕРЛІН, Я ЛЮБЛЮ ТЕБЕ",
+    "Rating": "16+",
+    "RatingDescription": "",
+    "Synopsis": "",
+    "ShortSynopsis": "",
+    "HOFilmCode": "F000000273",
+    "CorporateFilmId": "",
+    "RunTime": 120,
+    "OpeningDate": "2019-08-08T00:00:00",
+    "GraphicUrl": "",
+    "FilmNameUrl": "2019",
+    "TrailerUrl": "",
+    "AdditionalUrls": [],
+    "IsComingSoon": true,
+    "IsScheduledAtCinema": false,
+    "TitleAlt": "Berlin, I Love You ",
+    "RatingAlt": "",
+    "RatingDescriptionAlt": "",
+    "SynopsisAlt": "Німеччина",
+    "ShortSynopsisAlt": "Saban Films",
+    "WebsiteUrl": "",
+    "GenreId": "0000000030",
+    "EDICode": null,
+    "TwitterTag": "українська",
+    "TitleTranslations": [],
+    "SynopsisTranslations": [],
+    "ShortSynopsisTranslations": [],
+    "RatingDescriptionTranslations": [],
+    "CustomerRatingStatistics": {
+      "RatingCount": 0,
+      "AverageScore": null
+    },
+    "CustomerRatingTrailerStatistics": {
+      "RatingCount": 0,
+      "RatingCountLiked": 0
+    },
+    "FilmWebId": ""
+  },
+  {
+    "ID": "HO00000228",
+    "ShortCode": "",
+    "Title": "ТЕСТ",
+    "Rating": "18+",
+    "RatingDescription": "",
+    "Synopsis": "",
+    "ShortSynopsis": "",
+    "HOFilmCode": "F000000274",
+    "CorporateFilmId": "",
+    "RunTime": 100,
+    "OpeningDate": "2019-08-02T00:00:00",
+    "GraphicUrl": "",
+    "FilmNameUrl": "",
+    "TrailerUrl": "",
+    "AdditionalUrls": [],
+    "IsComingSoon": true,
+    "IsScheduledAtCinema": false,
+    "TitleAlt": "",
+    "RatingAlt": "",
+    "RatingDescriptionAlt": "",
+    "SynopsisAlt": "",
+    "ShortSynopsisAlt": "",
+    "WebsiteUrl": "",
+    "GenreId": "0000000005",
+    "EDICode": null,
+    "TwitterTag": "ТЕСТ",
+    "TitleTranslations": [],
+    "SynopsisTranslations": [],
+    "ShortSynopsisTranslations": [],
+    "RatingDescriptionTranslations": [],
+    "CustomerRatingStatistics": {
+      "RatingCount": 0,
+      "AverageScore": null
+    },
+    "CustomerRatingTrailerStatistics": {
+      "RatingCount": 0,
+      "RatingCountLiked": 0
+    },
+    "FilmWebId": ""
+  },
+];
+
+	
+   var Movie = function(Movies) {
+  Object.keys(Movies).map(key => {
+    this[key] = Movies[key];
+  });
+};
+
+Movie.prototype.getData = function(Title) {
+  return this[Title];
+};
+
+Movie.prototype.setData = function(Title, Movies) {
+  this[Title] = Movies;
+};
+
+Movie.prototype.deleteData = function(Title) {
+  delete this[Title];
+};
+
+
+// ****--------------
+
+
+var MoviesList = function(movies) {
+  this.movies = [];
+
+  if (movies.length > 0) {
+    for (var i = 0; i < movies.length; i++) {
+      this.movies.push(new Movie(movies[i]));
+    }
+  }
+};
+
+MoviesList.prototype.addMovie = function(Movies) {
+  this.movies.push(new Movie(Movies));
+};
+
+MoviesList.prototype.getMovies = function() {
+  return this.movies;
+};
+
+MoviesList.prototype.getMovieById = function(ID) {
+  if (this.movies.length === 0) return undefined;
+
+  for (var i = 0; i < this.movies.length; i++) {
+    if (this.movies[i].getData('ID') === ID) return this.movies[i];
+  }
+
+  return undefined;
+};
+
+MoviesList.prototype.findBy = function(property, value) {
+  if (this.movies.length === 0) return undefined;
+
+  for (var i = 0; i < this.movies.length; i++) {
+    if (this.movies[i].getData(property) === value) return this.movies[i];
+  }
+
+  return undefined;
+};
+
+MoviesList.prototype.sortBy = function(property, direction) {
+  if (this.movies.length === 0) return undefined;
+
+  this.movies.sort((a, b) => {
+    if(a.getData(property) === b.getData(property)) return 0;
+
+    if(direction === 'up') {
+      return a.getData(property) > b.getData(property) ? 1 : -1;
+    } else {
+      return a.getData(property) > b.getData(property) ? -1 : 1;
+    }
+  });
+
+  return this.getMovies();
+};
+
+MoviesList.prototype.deleteMovieById = function(ID) {
+  if (this.movies.length === 0) return undefined;
+
+  for (var i = 0; i < this.movies.length; i++) {
+    if (this.movies[i].getData('ID') === ID) this.movies.splice(i, 1);
+  }
+};
+
+
+  
+
+
+
+const myMoviesList = new MoviesList(Movies);
+
+const result = myMoviesList.sortBy('Rating', 'up');
+
+for (var i = 0; i < result.length; i++) {
+  console.log(result[i]);
 }
 
-function Actor ( name){
-
-	this.name = name ;
-	this.getName = function(){
-		return this.name;
-	}
-}
-
-function director ( name){
-
-	this.name = name ;
-	this.getName = function(){
-		return this.name;
-	}
 
 	
-}		
-
-
-function title ( name ){
-
-	this.name = name ;
-	this.getName = function(){
-		return this.name;
-	}
-
-}	
-
-function synopsis (name){
-	this.name = name ;
-	this.getName = function(){
-		return this.name;
-	}
-}
-
-
-	
-    
-    
-
-
-	
-// map
-// стрелочная функция
-
-
-
-
+  
